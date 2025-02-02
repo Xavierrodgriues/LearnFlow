81c0062b723f448dcb079272a865472bf1ff5bcc
Xavierrodgriues <xavierrodgriues123@gmail.com>
2025-02-03 02:58:15 +0530

    Starter template using react + electron

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+export default function App() {
+  return (
+    <>
+      <div>
+        <h1>
+            Desktop App
+        </h1>
+      </div>
+      <button>Get Started</button>
+    </>
+  );
+}
