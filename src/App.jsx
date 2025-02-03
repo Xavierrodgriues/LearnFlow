8636061ef5b6f77e21e07e3bf8f7e42a07718037
Xavierrodgriues <xavierrodgriues123@gmail.com>
2025-02-03 14:41:20 +0530

    New Node adding component

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ import {
 import "@xyflow/react/dist/style.css";
 
 const initialNodes = [
-  { id: "1", position: { x: 250, y: 5 }, data: { label: "1" } },
-  { id: "2", position: { x: 250, y: 100 }, data: { label: "2" } },
+  { id: "1", position: { x: 250, y: 5 }, data: { label: "1" } }
 ];
 
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
